c5f48b73a2466d97fdc62035adec6dd6ecfc18cf
ilnur <ilnur.nasybullin2000@yandex.ru>
2020-10-25 13:10:56 +0300

    added frontend - html + css

--- src/main/java/org/example/app/controllers/EmailController.java
+++ src/main/java/org/example/app/controllers/EmailController.java
@@ -1,12 +1,18 @@
 package org.example.app.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/email")
 public class EmailController {
+
+    @GetMapping
+    public String form() {
+        return "sign";
+    }
 
     @GetMapping("/something")
     public void something() {
